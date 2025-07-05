94815957db3340fbf65d89942d062d1c7e4c8ffc
joeltgn <joelsegurahernandez78@gmail.com>
2025-07-05 11:49:05 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Bienvenido a Lavandería</h1>
+      <p>La aplicación se ha desplegado correctamente.</p>
+    </div>
+  );
+}
 
